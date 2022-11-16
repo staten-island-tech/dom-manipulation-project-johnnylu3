af7b8158d207b7456e560d7a8c958472f7ea35ab
johnnylu3 <lujohnny517@gmail.com>
2022-11-16 10:11:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-const button = document.getElementById("btn");
-const text = document.querySelector("#text");
-console.log(button);
-
 const DOMSelectors = {
-  button: document.querySelector(".btn"),
+  submit: document.querySelector("#form"),
   text: document.querySelector("#text"),
   box: document.getElementById("big-black-box"),
   points: document.querySelectorAll(".point"), //node list
 };
 
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.textContent = "this is cool";
-  text.style.fontSize = "40px";
-}
-
-backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-
-DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+DOMSelectors.submit.addEventListener("submit", function (e) {
+  e.preventDefault();
+  e.createObject()
 });
 
-function changeLi() {
-  let pointIndex = 1;
-  DOMSelectors.points.forEach((point) => {
-    point.addEventListener("click", function () {
-      point.textContent = `Hello I am point ${pointIndex}`;
-      pointIndex++;
-    });
-  });
-}
-changeLi();
+const me = Object.create(person);
 
-DOMSelectors.form.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  console.log(input);
-});
-DOMSelectors.form.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  console.log(input);
-});
+querySelectorAll;
+};
+// DOMSelectors.form.addEventListener("click", function () {
+//   let input = DOMSelectors.input.value;
+//   console.log(input);
+// });
