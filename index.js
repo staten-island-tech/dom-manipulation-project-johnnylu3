b0d14e3e4718633df58143f8594e84d3a52d98d9
johnnylu3 <lujohnny517@gmail.com>
2022-11-16 20:41:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const DOMSelectors = {
   submit: document.querySelector("#form"),
   text: document.querySelector("#text"),
-  box: document.getElementById("big-black-box"),
+  box: document.querySelector(".form-parent"),
   points: document.querySelectorAll(".point"), //node list
+  button: document.querySelector(".btn"),
+  input: document.querySelector(".text-box"),
 };
 
 DOMSelectors.submit.addEventListener("submit", function (e) {
   e.preventDefault();
-  e.createObject()
 });
 
-const me = Object.create(person);
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value; // .value lets you get the values of an element, in this case something you typed in
 
-querySelectorAll;
-};
-// DOMSelectors.form.addEventListener("click", function () {
-//   let input = DOMSelectors.input.value;
-//   console.log(input);
-// });
+  console.log(input);
+
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `
+        <p class = 'extra-text'>${input}</p>
+    `
+  );
+});
+
+// -Create a function that creates an object and calls the following functions
+//-Create a function that injects the newly created object into the DOM
+//-Create a function that clears the input fields after injecting the object
+//-Create a function to remove an object after they have been created
