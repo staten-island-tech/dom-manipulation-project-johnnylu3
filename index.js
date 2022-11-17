758513f2781096e09bc2494c1ff515816057fd0e
johnnylu3 <lujohnny517@gmail.com>
2022-11-17 10:11:54 -0500

    we

--- index.js
+++ index.js
@@ -4,25 +4,40 @@ const DOMSelectors = {
   box: document.querySelector(".form-parent"),
   points: document.querySelectorAll(".point"), //node list
   button: document.querySelector(".btn"),
-  input: document.querySelector(".text-box"),
+  input1: document.querySelector(".text-box1"),
+  input2: document.querySelector(".text-box2"),
+  input3: document.querySelector(".text-box3"),
 };
 
 DOMSelectors.submit.addEventListener("submit", function (e) {
   e.preventDefault();
+  const input = DOMSelectors.input1.value;
+  const input2 = DOMSelectors.input2.value;
+  const input3 = DOMSelectors.input3.value; // .value lets you get the values of an element, in this case something you typed in
+  return { input, input2, input3 };
 });
 
-DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input.value; // .value lets you get the values of an element, in this case something you typed in
+// const { input1, input2, input3 } = getValues();
+// console.log(getValues);
 
-  console.log(input);
+// DOMSelectors.box.insertAdjacentHTML(
+//   "afterend",
+//   `
+//         <p class = 'extra-text'>${input}</p>
+//     `
+// );
 
-  DOMSelectors.box.insertAdjacentHTML(
-    "beforeend",
-    `
-        <p class = 'extra-text'>${input}</p>
-    `
-  );
-});
+function getNames() {
+  // get names from the database or API
+  let firstName = "John",
+    lastName = "Doe";
+
+  // return values
+  return {
+    firstName: firstName,
+    lastName: lastName,
+  };
+}
 
 // -Create a function that creates an object and calls the following functions
 //-Create a function that injects the newly created object into the DOM
