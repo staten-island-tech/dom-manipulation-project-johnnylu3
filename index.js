019eebe4eddd4e411911fffa90598cbd95facbdc
johnnylu3 <lujohnny517@gmail.com>
2022-11-30 18:52:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,44 +3,56 @@ const DOMSelectors = {
   text: document.querySelector("#text"),
   box: document.querySelector(".form-parent"),
   box2: document.querySelector(".display"),
-  points: document.querySelectorAll(".point"), //node list
-  button: document.querySelector(".btn"),
   input1: document.querySelector(".text-box1"),
   input2: document.querySelector(".text-box2"),
   input3: document.querySelector(".text-box3"),
 };
 
-let input1 = DOMSelectors.input1.value,
-input2 = DOMSelectors.input2.value,
-input3 = DOMSelectors.input3.value;
-let values = { input1, input2, input3 };
+function create() {
+  let input1 = DOMSelectors.input1.value,
+    input2 = DOMSelectors.input2.value,
+    input3 = DOMSelectors.input3.value;
 
-function insertCard (input1,input2, input3) { DOMSelectors.box2.insertAdjacentHTML(
-  "afterBegin",
-  `<div class="display-card">
+  card(input1, input2, input3);
+}
+
+function clear() {
+  DOMSelectors.input1.value = "";
+  DOMSelectors.input2.value = "";
+  DOMSelectors.input3.value = "";
+}
+
+function card(input1, input2, input3) {
+  DOMSelectors.box2.insertAdjacentHTML(
+    "afterBegin",
+    `<div class="display-card">
   <img class="display-img" src="${input3}"/>
   <h2 class="display-artist">${input2}</h2>
   <h3 class="display-album">${input1}</h3>
   <button class="remove btn">Remove Album</button>
 </div>
 `
-);}
+  );
+}
+function cardRemove() {
+  const removes = document.querySelectorAll(".remove");
+  removes.forEach((remove) => {
+    remove.addEventListener("click", function (removeBtn) {
+      removeBtn.target.parentElement.remove();
+    });
+  });
+}
 
 DOMSelectors.submit.addEventListener("submit", function (e) {
   e.preventDefault();
-insertCard();
-  DOMSelectors.input1.value = "";
-  DOMSelectors.input2.value = "";
-  DOMSelectors.input3.value = "";
-  querySelectorAll buttons
-removebutton.foreach 
-add event listner .target.parent Element.remove
-  let displaycard = document.querySelector("div.display-card"),
-    removeBtn = displaycard.querySelector("button.remove");
-
-  removeBtn.addEventListener("click", function () {
-    displaycard.remove();
-  });
+  create();
+  clear();
+  cardRemove();
 });
 
+// add event listner .target.parent Element.remove
+//   let displaycard = document.querySelector("div.display-card"),
+//     removeBtn = displaycard.querySelector("button.remove");
 
+//   removeBtn.addEventListener("click", function () {
+//     displaycard.remove();
